db62ebc2262cf215c9ac56ab8191ead69bce4c28
GustavoLima <47118393+Gustavin20012@users.noreply.github.com>
2023-01-01 22:40:16 -0300

    Usando referência em JS

---
+++ ex001/desafio002/main.js
@@ -0,0 +1,4 @@
+// Uma referência que armazene uma lista com todas as teclas e imprima o valor dela na aba console.
+
+const listaDeBotoes = document.querySelectorAll('input[type=button]');
+console.log(listaDeBotoes);
--- ex001/main.js
+++ ex001/main.js
@@ -2,4 +2,14 @@ function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+/*
+ Função que busca um único elemento ou class 
+'document.querySelector('.tecla_pom').onclick = tocaSomPom;'.
+*/
+
+// Referência para a função 'document.querySelectorAll('.tecla')'.
+const listaDeTeclas =  document.querySelectorAll('.tecla')
+
+// Chamando a referêcia 'listaDeTeclas' buscando o primeiro valor '[0]' e atribuindo '.onclick' e chamando a função 'tocaSomPom' para dar o play na musica.
+listaDeTeclas[0].onclick = tocaSomPom
