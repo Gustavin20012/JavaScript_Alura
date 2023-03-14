0de600de141678e25dfe37caa8aa468fa895a0ec
GustavoLima <47118393+Gustavin20012@users.noreply.github.com>
2023-03-14 20:25:29 -0300

    Update main.js

--- Robotron/ex001/js/main.js
+++ Robotron/ex001/js/main.js
@@ -21,21 +21,18 @@ const subtrair = document.querySelector("#subtrair")
 const somar =  document.querySelector("#somar")
 const braco = document.querySelector("#braco")
 
+// (const controle) faz a busca pela class ".controle-ajuste" e amarzena em "controle"
 const controle = document.querySelectorAll(".controle-ajuste")
 
-controle.forEach( () => {
-    elemento.addEventListener("click", () => {
-        console.log()
+controle.forEach( (elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent)
     })
 })
 
 // Funções que mostra a subtrações do elemento braco ao ser clicado
-somar.addEventListener("click", () => {manipulaDados("somar")})
-
-subtrair.addEventListener("click", (evento) =>{manipulaDados ("subtrair")})
-
 function manipulaDados (operacao) {
-    if (operacao === "subtrair") {
+    if (operacao === "-") {
         braco.value = parseInt(braco.value) - 1
     } else {
         braco.value = parseInt(braco.value) + 1
