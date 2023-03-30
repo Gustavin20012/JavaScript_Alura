7880149055d126a30a200f203ea476aabadb753d
GustavoLima <47118393+Gustavin20012@users.noreply.github.com>
2023-03-30 19:58:37 -0300

    desafio 003

--- Robotron/desafio003/main.js
+++ Robotron/desafio003/main.js
@@ -1,2 +1,23 @@
 // Criar um botão que faz aparecer a lista
 // Criar um segundo botão que faz com que a lista de cores volte a sumir
+
+
+function exibirTintas () {
+
+    
+}
+
+function ocultarTintas (evento) {
+    console.log(evento)
+    
+}
+
+
+function manipulaDados (operacao,) {
+
+    if (operacao === "exibir")
+        document.getElementsByClassName(lista).style.display = 'block';
+    else
+        document.getElementsByClassName(lista).style.display = 'none';
+}
+
