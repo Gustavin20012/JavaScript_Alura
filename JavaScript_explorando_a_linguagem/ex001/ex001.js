116c11fafe1e33b42ca7df1604a8589b84131b89
GustavoLima <47118393+Gustavin20012@users.noreply.github.com>
2023-05-31 19:37:59 -0300

    Update ex001.js

--- JavaScript_explorando_a_linguagem/ex001/ex001.js
+++ JavaScript_explorando_a_linguagem/ex001/ex001.js
@@ -1,1 +1,12 @@
-console.log("Hello, Word!");
+console.log("Meu Primeiro progrma: trabalhando com variáveis");
+
+const idade = 26;
+
+console.log("idade"); // Escrever no console.log dentro de "" aspas ele entendera que trata-se de um texto, sem as aspas ele vê como variavel.
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+
+const idadeSomada = idade+2;
+console.log(idadeSomada);
