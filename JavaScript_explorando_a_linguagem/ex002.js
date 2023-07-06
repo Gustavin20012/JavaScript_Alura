8979ff9de0166d986475ed2a897514f9cf20391f
GustavoLima <47118393+Gustavin20012@users.noreply.github.com>
2023-07-05 21:22:41 -0300

    JavaScript

---
+++ JavaScript_explorando_a_linguagem/ex002.js
@@ -0,0 +1,2 @@
+console.log("Trabalhando com variaveis");
+// liguagem JavaScript é uma liguagem "case sensitive" isso significa que letras maiúsculas e minusculas são diferentes para ele
---
+++ JavaScript_explorando_a_linguagem/ex005.js
@@ -0,0 +1,43 @@
+console.log("Atribuição de Variaveis");
+
+/*
+var
+    a variável var é declarada e iniciada no escopo da função
+    não respeitando bloco e permitindo a redeclaração e reatribuição.
+
+let
+    a variável let é declarada no escopo da função, mas só é inicializada posteriormente
+    respeitando bloco e permitindo reatribuição, mas não permite a redeclaração.
+
+const
+    a variável const é declarada no escopo da função, mas só é inicializada posteriormente
+    respeitando bloco e não permitindo reatribuição nem redeclaração.
+
+OBS 1: Ao declarar uma variável sem o var, sem o let ou sem o const, ela é criada no escopo global.
+*/
+
+/* 
+    No exemplo do (for) é ultilezado o (let x) que seu valor foi declado (x = 0), mas esse valor é reatribuido 9 vezes pelo contador.
+
+    Nesse mesmo exemplo a varivel x está respeitando o bloco, caso eu venha chamar ela fora do bloco seria uma nova variavel.
+*/
+
+for(let x = 0; 10 >= x; x++){
+    console.log(x);
+}
+
+const idade = 29;
+const primeiroNome = "Gustavo";
+const sobrenome = "Lima";
+
+
+//console.log(nome + " " + sobrenome);
+console.log(primeiroNome , sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`)
+
+
+let contador = 0
+contador = contador +1
+
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
