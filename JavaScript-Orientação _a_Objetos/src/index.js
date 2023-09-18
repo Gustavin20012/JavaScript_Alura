15c0f730672bd1ea424f7b23d69bff0745e65ba7
GustavoLima <47118393+Gustavin20012@users.noreply.github.com>
2023-09-18 20:40:31 -0300

    Update index.js

--- JavaScript-Orientação _a_Objetos/src/index.js
+++ JavaScript-Orientação _a_Objetos/src/index.js
@@ -1,10 +1,11 @@
-const personagemPedrinho = {
-    nome: 'Pedrinho',
-    vida: 7,
-    mana: 12,
-    level: 5,
-    tipo: 'Mago',
+/* const personagemPedrinho = {
+     nome: 'Pedrinho',
+     vida: 7,
+     mana: 12,
+     level: 5,
+     tipo: 'Mago',
 }
+*/ 
 
 const personagemJose = {
     nome: 'Jose',
@@ -13,4 +14,28 @@ const personagemJose = {
     level: 3,
     tipo: 'Arqueiro',
 }
+
+const personagemAna = {
+    nome: 'Ana',
+    vida: 8,
+    mana: 10, 
+}
+
+class Personagem {
+    nome
+    vida
+    mana
+    level
+    tipo
+    descricao
+}
+
+
+const personagemPedrinho = new Personagem()
+personagemPedrinho.nome = 'Pedrinho'
+personagemPedrinho.mana = 12
+personagemPedrinho.vida = 7
+personagemPedrinho.
+
+
 
