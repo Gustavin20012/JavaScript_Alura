0d488403caa67bc7c99e27da3170297faaacd36e
GustavoLima <47118393+Gustavin20012@users.noreply.github.com>
2023-10-05 18:56:17 -0300

    usando o this

--- JavaScript-Orientação _a_Objetos/src/index.js
+++ JavaScript-Orientação _a_Objetos/src/index.js
@@ -9,13 +9,14 @@ import { Personagem } from "./modules/personagem.js"
 }
 */ 
 
-const personagemJose = {
+/* const personagemJose = {
     nome: 'Jose',
     vida: 7,
     mana: 6,
     level: 3,
     tipo: 'Arqueiro',
-}
+ }
+ */
 
 const personagemAna = {
     nome: 'Ana',
@@ -27,8 +28,16 @@ const personagemPedrinho = new Personagem()
 personagemPedrinho.nome = 'Pedrinho'
 personagemPedrinho.mana = 12
 personagemPedrinho.vida = 7
+personagemPedrinho.tipo = 'Mago'
+personagemPedrinho.level = 5
 
-console.log(personagemPedrinho)
+const personagemJose = new Personagem()
+personagemJose.nome = 'Jose'
+personagemJose.tipo = 'Arqueiro'
+personagemJose.level = 3
+
+console.log('Insignia de ' +personagemPedrinho.nome + ':' +personagemPedrinho.obterInsignia())
+console.log('Insignia de ' +personagemJose.nome + ':' +personagemJose.obterInsignia())
 
 
 
