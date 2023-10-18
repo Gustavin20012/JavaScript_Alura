554bfbec7bce5d06c7b8b1e7b85a55707ebd7c2c
GustavoLima <47118393+Gustavin20012@users.noreply.github.com>
2023-10-18 20:46:48 -0300

    View
    Realizado a criação dos cartões

--- JavaScript-Orientação _a_Objetos/src/index.js
+++ JavaScript-Orientação _a_Objetos/src/index.js
@@ -1,16 +1,12 @@
 import { Personagem } from "./modules/personagem.js"
+import { PersonagemView } from "./components/personagem-view.js";
 
 const personagemPedrinho = new Personagem('Pedrinho', 5, 'Mago');
 const personagemJose = new Personagem('Jose', 3, 'Arqueiro');
 
-console.log(Personagem);
+const personagens = [personagemPedrinho, personagemJose]
 
-
-
-
-
-
-
+new PersonagemView(personagens).render()
 
 /*
 Primeira linha de codigo 
