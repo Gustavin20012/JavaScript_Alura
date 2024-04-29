1d7b3c3cb9bfbf9064e81a9db983bac9a107aeab
GustavoLima <47118393+Gustavin20012@users.noreply.github.com>
2024-04-29 18:39:52 -0300

    criando listar pensamento
    Criando listar pensamento e corrigindo o Criar pensamento, problema com o diretório realizado a deleção e criação de uma novo

--- memoteca/src/app/app.module.ts
+++ memoteca/src/app/app.module.ts
@@ -1,21 +1,21 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { RodapeComponent } from './componentes/rodape/rodape.component';
-import { CriarPensamentosComponent } from './componentes/pensamentos/criar-pensamentos/criar-pensamentos.component';
-import { FormsModule } from '@angular/forms';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     CabecalhoComponent,
     RodapeComponent,
-    CriarPensamentosComponent
+    CriarPensamentoComponent,
+    ListarPensamentoComponent
   ],
   imports: [
     BrowserModule,
