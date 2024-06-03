685ce46a8a7498ee04efa899382e1b51b4ef87b3
GustavoLima <47118393+Gustavin20012@users.noreply.github.com>
2024-06-03 20:48:19 -0300

    ngFor

--- memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
+  listaPensamento = [
+    
+  ];
   constructor() { }
 
   ngOnInit(): void {
