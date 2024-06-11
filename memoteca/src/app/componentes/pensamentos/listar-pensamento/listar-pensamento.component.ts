6f0e9b38134418275d802222630cf48f16c99d9f
GustavoLima <47118393+Gustavin20012@users.noreply.github.com>
2024-06-10 22:06:42 -0300

    ngClass

--- memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -18,6 +18,12 @@ export class ListarPensamentoComponent implements OnInit {
       conteudo: 'Minha propriedade é decorada com @Input()',
       autoria: 'Componente filho',
       modelo: 'modelo2'
+    },
+
+    {
+      conteudo: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam, esse quam consequuntur voluptates minima debitis maxime, accusantium ad necessitatibus a est id, quaerat provident cum facilis. Ratione eius voluptatum cum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima voluptates, ducimus, nulla quisquam ab quas veniam consequatur vel deserunt, quasi rem odio iste dolores dolore id accusamus eius iure quo. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut commodi, quas accusamus dolores sint officiis et rerum voluptas, dignissimos adipisci blanditiis temporibus cupiditate provident quia cum laboriosam deleniti in corrupti.',
+      autoria: '',
+      modelo: 'modelo1'
     }
   ];
   constructor() { }
