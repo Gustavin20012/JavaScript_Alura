2b1b6cc2e0f740e20b8ecc8bbc68c173a92f864c
GustavoLima <47118393+Gustavin20012@users.noreply.github.com>
2024-06-17 22:40:02 -0300

    Json serve

--- memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -7,26 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamento = [
-    {
-      conteudo: 'Passo informações para o componente filho',
-      autoria: 'Componente pai',
-      modelo: 'modelo3'
-    },
-
-    {
-      conteudo: 'Minha propriedade é decorada com @Input()',
-      autoria: 'Componente filho',
-      modelo: 'modelo2'
-    },
-
-    {
-      conteudo: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam, esse quam consequuntur voluptates minima debitis maxime, accusantium ad necessitatibus a est id, quaerat provident cum facilis. Ratione eius voluptatum cum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima voluptates, ducimus, nulla quisquam ab quas veniam consequatur vel deserunt, quasi rem odio iste dolores dolore id accusamus eius iure quo. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut commodi, quas accusamus dolores sint officiis et rerum voluptas, dignissimos adipisci blanditiis temporibus cupiditate provident quia cum laboriosam deleniti in corrupti.',
-      autoria: '',
-      modelo: 'modelo1'
-    }
-  ];
-  constructor() { }
+  listaPensamento: Pensamento[] = [];
+  constructor() {}
 
   ngOnInit(): void {
   }
